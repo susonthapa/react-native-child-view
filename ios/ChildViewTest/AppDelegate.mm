67471af34e65c8f7116acc38aae8c5a333070c6b
susonthapa <33973551+susonthapa@users.noreply.github.com>
2023-10-14 08:52:50 +0545

    added project files

--- ios/ChildViewTest/AppDelegate.mm
+++ ios/ChildViewTest/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+@import GoogleMaps;
 
 @implementation AppDelegate
 
@@ -10,6 +11,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  [GMSServices provideAPIKey:@"YOUR_KEY"];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
